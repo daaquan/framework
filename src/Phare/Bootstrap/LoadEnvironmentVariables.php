2c2fcff9a896d3e8e86cfae96bef39d7a4e1c58a
daaquan <daaquan@gmail.com>
2025-07-12 01:07:16 +0900

    bugfix

--- src/Phare/Bootstrap/LoadEnvironmentVariables.php
+++ src/Phare/Bootstrap/LoadEnvironmentVariables.php
@@ -22,9 +22,6 @@ class LoadEnvironmentVariables
      */
     public function bootstrap(Application $app): void
     {
-        (new Dotenv())->usePutenv()
-            ->load($app->basePath() . '/.env');
-        return;
         try {
             (new Dotenv())->usePutenv()
                 ->load($app->basePath() . '/.env');
